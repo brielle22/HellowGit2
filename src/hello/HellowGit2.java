7f985ea41cf5dea74efabfb45dd508d0cbcca1c1
Brielle <snobbybrielle@gmail.com>
2017-01-19 15:34:18 +1300

    Initial checking Test

---
+++ src/hello/HellowGit2.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HellowGit2 {
+	public static void main (String[] args){
+			for(int i=1;i<=10;i++) {
+				System.out.println("Hello World.." +i);
+			}
+	
+	
+	
+	}
+}
